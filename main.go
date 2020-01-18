131ffaf73aa71a8a09cf0aa5114125b704deefe5
Bhageerath Reddy <bhageerathreddyy@gmail.com>
2020-01-18 17:14:28 +0530

    Update main.go
    Added basic HTTP Server logic

--- main.go
+++ main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
+func requestHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(responseWriter, "Hi there! The URL path is %s!", request.URL.Path)
+}
+
 func main() {
-	fmt.Println("An application to count lines of a GitHub repository!")
+	log.Println("An application to count lines of a GitHub repository!")
+	http.HandleFunc("/", requestHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
