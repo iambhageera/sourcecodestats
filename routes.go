8bb9ad61a7856bf04ec0a353e34193d63cbb7521
Bhageerath Reddy <bhageerathreddyy@gmail.com>
2020-01-19 18:38:50 +0530

    Cleaned up service specific logic
    Moved the logic to separate files in the previous commit

--- routes.go
+++ routes.go
@@ -1,23 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"net/http"
 	"regexp"
 )
 
-// GitHubURLPattern - Regex Pattern for GitHub request URLs
-var GitHubURLPattern = `^/github/([^/]+)/([^/]+)/?$`
-
-// GitLabURLPattern - Regex Pattern for GitLab request URLs
-var GitLabURLPattern = `^/gitlab/([^/]+)/([^/]+)/?$`
-
-// BitBucketURLPattern - Regex Pattern for BitBucket request URLs
-var BitBucketURLPattern = `^/bitbucket/([^/]+)/([^/]+)/?$`
-
-// Route - Structure to store details of a route such as URL pattern
-// and it's handler
+// Route - Structure to store details of a route
 type Route struct {
 	urlPattern *regexp.Regexp
 	handler    http.Handler
@@ -63,79 +51,4 @@ func InitializeRouteHandleManager() *HTTPRouteHandleManager {
 	var routeHandler *HTTPRouteHandleManager = new(HTTPRouteHandleManager)
 	routeHandler.routes = make([]*Route, 0)
 	return routeHandler
-}
-
-// GithubHandler - Request handler type for GitHub
-type GithubHandler struct{}
-
-func (handler GithubHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-
-	var registrar = ServerSideEventRegistrar{
-		messageCounter: 0,
-		request:        request,
-		connection:     responseWriter,
-	}
-
-	if ok, err := registrar.PrepareConnection(); !ok {
-		http.Error(responseWriter, err.message, http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(responseWriter, "Hello from GitHub! %q", html.EscapeString(request.URL.Path))
-}
-
-// GitlabHandler - Request handler type for GitLab
-type GitlabHandler struct{}
-
-func (handler GitlabHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-
-	var registrar = ServerSideEventRegistrar{
-		messageCounter: 0,
-		request:        request,
-		connection:     responseWriter,
-	}
-
-	if ok, err := registrar.PrepareConnection(); !ok {
-		http.Error(responseWriter, err.message, http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(responseWriter, "Hello from GitLab! %q", html.EscapeString(request.URL.Path))
-}
-
-// BitbucketHandler - Request handler for BitBucket
-type BitbucketHandler struct{}
-
-func (handler BitbucketHandler) ServeHTTP(responseWriter http.ResponseWriter, request *http.Request) {
-
-	var registrar = ServerSideEventRegistrar{
-		messageCounter: 0,
-		request:        request,
-		connection:     responseWriter,
-	}
-
-	if ok, err := registrar.PrepareConnection(); !ok {
-		http.Error(responseWriter, err.message, http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(responseWriter, "Hello from BitBucket! %q", html.EscapeString(request.URL.Path))
-}
-
-// GetGitHubHandler - Gets the route handler for GitHub
-func GetGitHubHandler() *Route {
-	var githubRegex *regexp.Regexp = regexp.MustCompile(GitHubURLPattern)
-	return &Route{githubRegex, GithubHandler{}}
-}
-
-// GetGitLabHandler - Gets the route handler type for GitLab
-func GetGitLabHandler() *Route {
-	var gitlabRegex *regexp.Regexp = regexp.MustCompile(GitLabURLPattern)
-	return &Route{gitlabRegex, GitlabHandler{}}
-}
-
-// GetBitBucketHandler - Gets the route handler for BitBucket
-func GetBitBucketHandler() *Route {
-	var bitbucketRegex *regexp.Regexp = regexp.MustCompile(BitBucketURLPattern)
-	return &Route{bitbucketRegex, BitbucketHandler{}}
 }
