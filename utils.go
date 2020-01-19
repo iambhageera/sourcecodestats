68cd630c4d752f0c0ab7c3f835b4e96d6ff03622
Bhageerath Reddy <bhageerathreddyy@gmail.com>
2020-01-19 20:26:01 +0530

    Added logic to get client IP address

--- utils.go
+++ utils.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"net/http"
+	"strings"
+)
+
 // Error - represents an error
 type Error struct {
 	message string
@@ -7,4 +12,23 @@ type Error struct {
 
 func (error *Error) Error() string {
 	return error.message
+}
+
+// GetClientIPAddress - Gets the client IP address from the request headers
+func GetClientIPAddress(request *http.Request) string {
+
+	if request == nil {
+		return ""
+	}
+
+	forwarded := request.Header.Get("X-FORWARDED-FOR")
+
+	if len(forwarded) == 0 {
+		return request.RemoteAddr
+	}
+
+	// forwarded may contain multiple IPs, get the first one
+	ips := strings.Split(forwarded, ", ")
+
+	return ips[0]
 }
