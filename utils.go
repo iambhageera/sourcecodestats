5ea94b5be8fa254e5c4c39de1887b300d93926f2
Bhageerath Reddy <bhageerathreddyy@gmail.com>
2020-01-19 18:37:44 +0530

    Moved Repository to separate file

--- utils.go
+++ utils.go
@@ -8,10 +8,3 @@ type Error struct {
 func (error *Error) Error() string {
 	return error.message
 }
-
-// Repository - Stores the information about the
-type Repository struct {
-	name    string
-	service string
-	owner   string
-}
